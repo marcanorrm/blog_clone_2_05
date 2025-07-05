d892c8ca224eb40f05089b2056d677ce6844edd6
marcanorrm <gerardozrmm@gmail.com>
2025-07-05 16:41:42 -0400

    login

--- django_base/settings.py
+++ django_base/settings.py
@@ -124,3 +124,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login URL
+LOGIN_REDIRECT_URL = 'post-list'
